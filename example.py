d83fdf6675b968fe4e186eb60a3c928af76699b1
Cecile Murray <cecile.m.murray@census.gov>
2022-02-02 17:40:42 -0500

    add print_repeatedly() to example.py

--- example.py
+++ example.py
@@ -1,1 +1,15 @@
 print("Hello, GASP attendees!")
+
+def print_repeatedly(s, N):
+    '''
+    Print specified string N times
+
+    Takes:
+    - string to print
+    - int number of times to print it
+    Returns:
+    - None
+    '''
+
+    for i in range(N):
+        print(s)
